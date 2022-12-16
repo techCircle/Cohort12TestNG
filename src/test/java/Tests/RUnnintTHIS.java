76b7fbe1f42c62732dc2e315496011fca77e9db3
techCircle <53327631+techCircle@users.noreply.github.com>
2022-12-15 20:10:54 -0500

    added testng annotation

--- src/test/java/Tests/RUnnintTHIS.java
+++ src/test/java/Tests/RUnnintTHIS.java
@@ -1,8 +1,12 @@
 package Tests;
 
+import org.testng.annotations.Test;
+
 public class RUnnintTHIS {
 
-	public static void main(String[] args) {
+	
+	@Test
+	public void test1() {
 		System.out.println("JENKINS HELLO WORLD");
 
 	}
